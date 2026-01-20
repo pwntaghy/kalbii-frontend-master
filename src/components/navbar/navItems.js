f9a871e0df467fd05d6675fea16d0a903b86953d
pwntaghy <pwnthakur84@gmail.com>
2026-01-20 13:20:56 +0530

    Update feature / fix bug

--- src/components/navbar/navItems.js
+++ src/components/navbar/navItems.js
@@ -1,6 +1,6 @@
 const navItems = [
     { label: "nitin", path: "/" },
-    { label: "devops", path: "/" },
+    { label: "thakur", path: "/" },
     // {label:"Portfolio",path:"/portfolio"},
     // {label:"Blogs",path:"/blogs"},
     { label: "hi", path: "/" },
